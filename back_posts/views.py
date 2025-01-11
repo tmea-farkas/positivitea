d6b2c5eeb5d7e0953ea9dd7113c1656329abebbf
Timea Farkas <tmeafarkas@yahoo.com>
2025-01-11 18:45:13 +0000

    tried adding filters and more fields to profiles and posts; unsuccessful

--- back_posts/views.py
+++ back_posts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from backend_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -8,6 +9,19 @@ class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Post.objects.all()
+    filter_backends = [
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
