d5b0e2ab3e63adfd2d2a12d589034ff2d081ca20
Timea Farkas <tmeafarkas@yahoo.com>
2025-01-12 15:09:34 +0000

    added naturaltime to messages and comments to ensure human readability plus pagination

--- back_messages/serializers.py
+++ back_messages/serializers.py
@@ -1,13 +1,22 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers, generics
 from .models import Message
 
 class MessageSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context.get('request')
         return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
     def validate_content(self, value):
         if not value.strip():
