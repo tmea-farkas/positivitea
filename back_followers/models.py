c96bba244a71a4db2053e9ffba702e53a9721ba0
Timea Farkas <tmeafarkas@yahoo.com>
2025-01-13 16:10:00 +0000

    added additional filelds to the profile

--- back_followers/models.py
+++ back_followers/models.py
@@ -4,7 +4,7 @@ from back_profiles.models import Profile
 
 class Follower(models.Model):
     owner = models.ForeignKey(
-        User, related_name='follower', on_delete=models.CASCADE)
+        User, related_name='following', on_delete=models.CASCADE)
     followed = models.ForeignKey(
         User, related_name='followed', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
