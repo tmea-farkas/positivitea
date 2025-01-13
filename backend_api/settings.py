3d7c97c6674e606a62186f3b3e0288c0bfb9789c
Timea Farkas <tmeafarkas@yahoo.com>
2025-01-13 13:57:04 +0000

    fixed setting.py typo

--- backend_api/settings.py
+++ backend_api/settings.py
@@ -41,9 +41,9 @@ REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%d %b %Y',
 }
 if 'DEV' not in os.environ:
-    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = {
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
         'rest_framework.renderers.JSONRenderer',
-    }
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
