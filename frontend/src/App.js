7ac52bec45592708c0ae56476a9cd38791cf5186
Timea Farkas <tmeafarkas@yahoo.com>
2025-01-14 19:04:49 +0000

    added routing to pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import { Route, Switch } from 'react-router-dom';
+
 
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={()=> <h1>Home</h1>} />
+          <Route exact path="/signin" render={()=> <h1>Sign In</h1>} />
+          <Route exact path="/register" render={()=> <h1>Register</h1>} />
+          <Route render={()=><p>Page not found!</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
