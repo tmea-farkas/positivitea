9904216a79230f4c1835d96e9c5b847bf79188db
Timea Farkas <tmeafarkas@yahoo.com>
2025-01-17 15:51:47 +0000

    added post form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import { Route, Switch } from 'react-router-dom';
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
+import PostCreateForm from './pages/posts/PostCreateForm';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="/" render={()=> <h1>Home</h1>} />
           <Route exact path="/signin" render={() => <SignInForm/>} />
           <Route exact path="/register" render={()=> <SignUpForm/>} />
+          <Route exact path="/posts/create" render={()=> <PostCreateForm/>} />
           <Route render={()=><p>Page not found!</p>} />
         </Switch>
       </Container>
