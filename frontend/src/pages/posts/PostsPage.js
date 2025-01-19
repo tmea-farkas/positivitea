0105aafb429ff3e33473f9df14fc803abf4c905d
Timea Farkas <tmeafarkas@yahoo.com>
2025-01-19 17:38:21 +0000

    added the search bar, infinite scroll and updated the logo

--- frontend/src/pages/posts/PostsPage.js
+++ frontend/src/pages/posts/PostsPage.js
@@ -3,22 +3,26 @@ import { Col, Row, Container, Form } from 'react-bootstrap';
 
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 import Post from './Post';
 import Asset from '../../components/Asset';
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
 import noresults from '../../assets/no-results.png';
+import {fetchMoreData} from '../../utils/utils';
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const {pathname}= useLocation();
 
+  const [query, setQuery] = useState('');
+
   useEffect(() => {
     const fetchPosts = async () => {
         try {
-          const {data} = await axiosReq.get(`/posts/?${filter}`)
+          const {data} = await axiosReq.get(`/posts/?${filter}search=${query}`)
           setPosts(data);
           setHasLoaded(true)
         } catch(err){
@@ -26,19 +30,42 @@ function PostsPage({ message, filter = "" }) {
         }
     }
     setHasLoaded(false);
-    fetchPosts();
-  }, [filter, pathname]);
+    const timer = setTimeout(() => {
+      fetchPosts();
+    }, 1000)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [filter, query, pathname]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
+        <i className={`fas fa-search ${styles.SearchIcon}`}/>
+        <Form className={styles.SearchBar}
+        onSubmit={(event) => event.preventDefault()}>
+          <Form.Control 
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+          type="text" 
+          className="mr-sm-2"
+          placeholder="Search posts"/>
+        </Form>
         {hasLoaded ? (
           <>
           {posts.results.length ? (
-            posts.results.map(post => (
-              <Post key={post.id} {...post} setPosts={setPosts}/>
-            ))
+            <InfiniteScroll
+              children={
+                posts.results.map(post => (
+                  <Post key={post.id} {...post} setPosts={setPosts}/>
+                ))
+              }
+              dataLength={posts.results.length}
+              loader={<Asset spinner/>}
+              hasMore={!!posts.next}
+              next={() => fetchMoreData(posts, setPosts)}
+            />
           ) : (
             <Container className={appStyles.Content}>
               <Asset src={noresults} message={message} />
